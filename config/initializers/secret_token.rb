9a0f82fdb9e64e31c1ac37b89ffd83c75afd5635
Spencer Reinke <reinke.spencer@gmail.com>
2014-09-09 16:36:46 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pdfhtml::Application.config.secret_key_base = '84c0e9710ce86beb09f4157d297ff468f6aba1e07f46bb22aa965751e7b66c3c4eb63c7a88c214944fe3d424c8707d6d38f28fac9191c9843bef441549808afa'
